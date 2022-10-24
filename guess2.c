c5ab8308b4a3c5c73cb1c6147606117c748f5f1d
Eugene Kim <eugenekim@cpp.edu>
2022-10-23 22:25:04 -0700

    Init commit with comments

---
+++ guess2.c
@@ -0,0 +1,48 @@
+/*
+
+    Program: guess.c
+
+    This is interactive program which gets input from the user to geuss
+    a number between 1-10. 
+
+    Menu should look like this: 
+        1. Press 1 to play game
+            * promt the user to enter a number for guess
+            * If correct, tell them they won and back to main menu
+            * Else, tell them number is too low or high, and let them guess again
+            * This should continue until they win
+            * 
+            * Input type: number, others are invaild and try again, q is quit
+        2. Press 2 to change the max number
+            * let the user set the max value (within 1-10)
+        3. Press 3 to quit
+            * Thank the user and end the game
+
+*/
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h>
+
+// global and static variables
+
+
+// Function prototypes
+
+int main()
+{
+
+    // variable declare here
+
+
+    // seed the time in random, generate target
+
+
+    // actual game with do-while loop
+
+
+
+
+
+
+
+}
