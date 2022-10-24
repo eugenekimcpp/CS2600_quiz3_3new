5ffa8f2019c92b0ced56fa52996e8bdf75ffcd8f
Eugene Kim <eugenekim@cpp.edu>
2022-10-23 22:49:08 -0700

    programs run w few error

--- guess2.c
+++ guess2.c
@@ -24,25 +24,141 @@
 #include <time.h>
 
 // global and static variables
-
+int guess;
+int max = 10;
 
 // Function prototypes
+void displayMenu();
+void suggestGuess(int target);
+int promptUser();
+int checkGuess(int target); // 1 true, 0 false
+
 
 int main()
 {
 
     // variable declare here
-
+    int target; 
+    int menu;
+    
+    // var as boolean
+    int keepProgram = 1;
+    int keepGame = 1;
+    int validInput = 0;
+    int check = 0; 
 
     // seed the time in random, generate target
-
+    time_t t;
+    srand((unsigned) time(&t));
+    target = (rand() % 10) + 1;
 
     // actual game with do-while loop
+    do{
+        // new game start
+        // reset variables
+        guess = 0;
+        
+        displayMenu();
+        menu = promptUser();
+
+        switch(menu)
+        {
+
+            case 1: 
+                // actual game
+                // loops until target found, or q pressed
+                while(keepGame == 1)
+                {
+                    printf("Please enter a number to guess: ");
+                    validInput = scanf("%d", &guess);
+                    if(validInput == 1)
+                    {
+                        // enter here when valid integer was received
+                        if((guess > 0) && (guess < max))
+                        {
+                            // when user inputs correct range of guess
+                            check = checkGuess(target);
+                            if(check == 1)
+                            {
+                                printf("Your guess is correct!!\n");
+                                keepGame = 0;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        keepGame = 0;
+                    }
+
+                }
+                break;
+            case 2: 
+                // change max num
+                printf("set new max, 1 - 10: ");
+                scanf("%d", &max);
+                keepProgram = 1;
+                break;
+            case 3: 
+                printf("Thank you for playing this game! \n");
+                keepProgram = 0;
+                break; 
+            default:
+                printf("Invalid menu input, try again...\n");
+                keepProgram = 1;
+                break;
+        }
+
+        
+
+
+
+    } while (keepProgram == 1);
 
 
 
 
 
 
+}
 
+void displayMenu()
+{
+    printf("Press 1 to play a game\n");
+    printf("Press 2 tp change the max number\n");
+    printf("Press 3 to quit\n");
+
+}
+
+void suggestGuess(int target)
+{
+    if(guess < target) 
+    {
+        printf("The guess is too low\n");
+    }
+    else
+    {
+        printf("The guess is too high\n"); 
+    }
+}
+
+int promptUser()
+{
+    int choice; 
+    printf("Please enter your choice: ");
+    scanf("%d", &choice);
+    return choice; 
+
+}
+
+int checkGuess(int target) 
+{
+    if (guess == target)
+    { 
+        return 1; 
+    }
+    else 
+    {
+        suggestGuess(target);
+        return 0; 
+    }
 }
